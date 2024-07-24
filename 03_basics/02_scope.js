e1e2595516ddd36048a52d992f011a0537e4c9ef
izshivam <shivamkumarchoudhary8116@gmail.com>
2024-07-24 13:25:59 +0530

    scope & hoisting completed

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,133 @@
+// let a = 10;
+// const b = 20;
+// var c = 30;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+// if (true) {
+//     let a = 10;
+//     const b = 20;
+//     var c = 30;
+// }
+
+// // console.log(a);
+// // console.log(b);
+// console.log(c);
+
+
+
+// var c = 300;
+// if (true) {
+//     let a = 10;
+//     const b = 20;
+//     var c = 30;
+// }
+
+// // console.log(a)
+// // console.log(b)
+// console.log(c)
+
+
+
+// let a = 300;
+// if (true) {
+//     let a = 10;
+//     const b = 20;
+//     var c = 50;
+// }
+// console.log(a);
+
+
+
+// let a = 300;
+// if (true) {
+//     let a = 10;
+//     const b = 20;
+//     var c = 50;
+//     console.log("INNER: ", a);
+// }
+// console.log(a);
+
+
+/************************************************************/
+// function one(){
+//     const username = "Shivam"
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     // console.log(website);
+//     two()
+// }
+// one()
+
+
+
+// function one(){
+//     const username = "Shivam"
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     two()
+// }
+// one()
+
+
+
+// if (true) {
+//     const username = "Shivam"
+//     if (username === "Shivam") {
+//         const website = " youtube"
+//         console.log(username + website);
+//         console.log(website);
+//     }
+// }
+// console.log(username);
+
+
+
+// if (true) {
+//     const username = "Shivam"
+//     if (username === "Shivam") {
+//         const website = " youtube"
+//         console.log(username + website);    
+//     }
+// }
+
+
+
+// function addone(num) {
+//     return num + 1
+// }
+// addone(5)
+
+// function addtwo(num) {
+//     return num + 2
+// }
+// addtwo(5)
+
+
+
+// addone(5)
+// function addone(num) {
+//     return num + 1
+// }
+
+
+
+// console.log(addone(5))
+// function addone(num) {
+//     return num + 1
+// }
+
+
+
+// addtwo(5)
+// const addtwo = function(num) {
+//     return num + 2
+// }
