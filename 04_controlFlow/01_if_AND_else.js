c8426e22ef6b9bfc58877f237978993fc5edd7c0
izshivam <shivamkumarchoudhary8116@gmail.com>
2024-07-27 00:48:41 +0530

    if AND else, switch, truthy that is control flow completed

---
+++ 04_controlFlow/01_if_AND_else.js
@@ -0,0 +1,101 @@
+// if
+
+// if (true) {
+    
+// }
+// if (false) {
+    
+// }
+
+
+if ( 2 === 2 ) {
+    
+}
+
+// if ( 2 == "2" ) {
+//     console.log("ecetuted");
+// }
+
+
+// if ( 2 != 3 ) {
+//     console.log("ecetuted");
+// }
+
+
+const temp = 41
+// if (temp < 50) {
+//     console.log("less than 50");
+// } 
+// console.log("Greater than 50");
+
+
+// if (temp === 41 ) {
+//     console.log("less than 50");
+// } 
+// console.log("Greater than 50");
+
+
+// if ( temp === 41 ) {
+//     console.log("less than 50");
+// } else {
+//      console.log("greater than 50");
+//     } 
+// console.log("executed");
+
+
+// if ( temp === 40 ) {
+//     console.log("less than 50");
+// } else {
+//      console.log("greater than 50");
+//     } 
+// console.log("executed");
+
+
+// const score = 200
+// if ( score > 100 ) {
+//     const Power = "fly"
+//     console.log(`User Power: ${Power}`);
+// }
+// console.log(`User Power: ${Power}`);
+
+
+// const score = 200
+// if ( score > 100 ) {
+//     const Power = "fly"
+//     console.log(`User Power: ${Power}`);
+// }
+
+
+// const score = 200
+// if ( score > 100 ) {
+//     var Power = "fly"
+//     console.log(`User Power: ${Power}`);
+// }
+// console.log(`User Power: ${Power}`);
+
+
+// const balance = 1000
+// // if ( balance < 500 ) console.log("test"), console.log("test2");
+// if ( balance < 500 ) {
+//     console.log("less than 500");
+// } else if ( balance < 750 ) {
+//     console.log("less than 750");
+// } else if ( balance < 950 ) {
+//     console.log("less than 950");
+// } else {
+//     console.log("less than 1200");
+// }
+
+
+const useLoggedIn = true
+const debitcard = true
+// if (useLoggedIn && debitcard) {
+//     console.log("Allow to buy course");
+// }
+
+
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+// if (loggedInFromEmail || loggedInFromGoogle) {
+//     console.log("Allow to buy");
+// }
