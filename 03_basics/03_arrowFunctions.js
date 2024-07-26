f339c0c56de042bd81327bf92b9c58b29c9a3ef0
izshivam <shivamkumarchoudhary8116@gmail.com>
2024-07-26 23:30:12 +0530

    function, arrow function, iffe completed

---
+++ 03_basics/03_arrowFunctions.js
@@ -0,0 +1,117 @@
+// const user = {
+//     username: "Shivam",
+//     price: 999,
+
+//     welcomeMessage: function() {
+//         console.log(`${this.username} , welcome to website`);
+//     }
+    
+// }
+
+// user.welcomeMessage()
+// user.username = "Sam"
+// user.welcomeMessage()
+
+
+
+// const user = {
+//     username: "Shivam",
+//     price: 999,
+
+//     welcomeMessage: function() {
+//         console.log(`${this.username} , welcome to website`);
+//         console.log(this);
+//     }
+    
+// }
+
+// user.welcomeMessage()
+// user.username = "Sam"
+// user.welcomeMessage()
+
+
+
+// const user = {
+//     username: "Shivam",
+//     price: 999,
+
+//     welcomeMessage: function() {
+//         console.log(`${this.username} , welcome to website`);
+//         console.log(this);
+//     }
+    
+// }
+
+// console.log(this);
+
+
+
+// function chai() {
+//     console.log(this);
+// }
+// chai()
+
+
+
+// function chai() {
+//     let username = "Shivam"
+//     console.log(this);
+// }
+// chai()
+
+
+
+// function chai() {
+//     let username = "Shivam"
+//     console.log(this.username);
+// }
+// chai()
+
+
+
+// const chai = function () {
+//     let username = "Shivam"
+//     console.log(this.username);
+// }
+// chai()
+
+
+
+// const chai = () => {
+//     let username = "Shivam"
+//     console.log(this.username);
+// }
+// chai()
+
+
+
+// const chai = () => {
+//     console.log(this);
+// }
+// chai()
+
+
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+// console.log(addTwo(5, 3));
+
+
+
+// const addTwo = (num1, num2) => num1 + num2
+// console.log(addTwo(5, 3));
+
+
+
+// const addTwo = (num1 , num2) => (num1 + num2)
+// console.log(addTwo(5, 6));
+
+
+
+// const addTwo = (num1, num2) => {username: "Shivam"}
+// console.log(addTwo(3, 5));
+
+
+// const addTwo = (num1 , num2) => ({username: "Sam"})
+// console.log(addTwo(4, 5));
---
+++ 03_basics/04_iife.js
@@ -0,0 +1,21 @@
+// function chai () {
+//     console.log(`DB Connected`);
+// }
+// chai()
+
+
+
+// (function chai () {
+//     console.log(`DB Connected`);
+// })()
+
+
+
+// ( () => {
+//     console.log(`DB Connected`);
+// }) ()
+
+
+// ( (name) => {
+//     console.log(`DB Connected ${name}`);
+// }) ('Shivam')
